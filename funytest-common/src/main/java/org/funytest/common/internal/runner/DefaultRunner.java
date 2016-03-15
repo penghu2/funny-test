d52d7409f3533426dd911472a2331f235207fccf
xiuzhu.hp <hupeng@10.15.236.137>
2016-03-15 21:20:17 +0800

    提交代码

--- funytest-common/src/main/java/org/funytest/common/internal/runner/DefaultRunner.java
+++ funytest-common/src/main/java/org/funytest/common/internal/runner/DefaultRunner.java
@@ -1,12 +1,17 @@
 package org.funytest.common.internal.runner;
 
+import java.util.Map;
+
 import org.funytest.common.internal.ITestRunner;
 import org.funytest.common.model.TestContext;
 
 public class DefaultRunner implements ITestRunner {
+	
+	public Map<String, String> testStepHandlerMap;
 
 	public void run(TestContext context) {
-		// TODO Auto-generated method stub
+		
+		/* 开始执行用例 */
 		
 	}
 
